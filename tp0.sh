42e2aea1cfe48622af53b227ed0d7c6d6e157314
nutria <nutria@aspr3.home>
2018-09-21 14:15:56 +0100

    some mods from the originals, untested, hope they work!

---
+++ tp0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script disables the Synaptics Touchpad using the xinput command in Linux X-Windows.
+# we need hte xinput command
+command -v xinput
+if [ $? -eq 1 ]
+then
+  echo "Sorry, you need the xinput command for this script, in ubuntu install via 'apt-get install xinput'"
+  exit
+fi
+# first we collect internal identification of our touchpad device
+TPD=`xinput list | sed -ne 's/^.\+Touch[Pp]ad \+\tid=\([0-9]\+\).\+/\1/p'`
+#then we render it "0" which means disabled.
+xinput set-prop $TPD "Device Enabled" 0
